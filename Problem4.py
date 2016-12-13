d3bdab77ab40b34222a5cc4ee46ca76b3e7824c8
gol3tron <adamgoler@gmail.com>
2016-12-13 15:25:25 -0800

    solved 4

--- Problem4.py
+++ Problem4.py
@@ -4,13 +4,31 @@
 
 def isPalindrome(number):
 
+    # this function only works for numbers with an even number of digits
+
     str_num = str(number)
 
     length_of_num = len(str_num)
 
     firsthalf = str_num[0:int(length_of_num/2)]
-    secondhalf = str_num[int(length_of_num/2)+1:len(length_of_num)]
+    secondhalf = str_num[int(length_of_num/2):length_of_num]
 
-    if firsthalf==secondhalf:
+    if firsthalf==secondhalf[::-1]:
         return True
+    else:
+        return False
+
+
+def searchSet(test):
+    
+    for i in range(900,1000):
+        if(isPalindrome(test*i)):
+            print(test*i)
+            break
+        if (i==999):
+            print("none")
+
+
+for i in range(900,1000):
+    searchSet(i)
 
